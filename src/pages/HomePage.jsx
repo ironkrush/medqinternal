271d60e6f0b49c4ea1c2546a42093b3c14015922
ironkrush <krush2752@gmail.com>
2024-08-29 16:04:41 +0530

    homepage get started problem solved also footer aligned

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -135,34 +135,34 @@ const HomePage = () => {
                     </div>
                     <div className="medi-img">
                         <div className="tagline">
-                        <Link to='/signup'>
-                            <button className="hbutton">
-                                
-                                <p className="hbutton__text">
-                                    <span style={{ '--index': 0 }}>G</span>
-                                    <span style={{ '--index': 1 }}>E</span>
-                                    <span style={{ '--index': 2 }}>T</span>
-                                    <span style={{ '--index': 3 }}></span>
-                                    <span style={{ '--index': 4 }}>S</span>
-                                    <span style={{ '--index': 5 }}>T</span>
-                                    <span style={{ '--index': 6 }}>A</span>
-                                    <span style={{ '--index': 7 }}>R</span>
-                                    <span style={{ '--index': 8 }}>T</span>
-                                    <span style={{ '--index': 9 }}>E</span>
-                                    <span style={{ '--index': 10 }}>D</span>
-                                    <span style={{ '--index': 11 }}> </span>
-                                </p>
-                              
-                                <div className="hbutton__circle">
-                                    <svg viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="hbutton__icon" width="14">
-                                        <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" fill="currentColor"></path>
-                                    </svg>
-                                    <svg viewBox="0 0 14 15" fill="none" width="14" xmlns="http://www.w3.org/2000/svg" className="hbutton__icon hbutton__icon--copy">
-                                        <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" fill="currentColor"></path>
-                                    </svg>
-                                    
-                                </div>
-                            </button>
+                            <Link to='/signup'>
+                                <button className="hbutton">
+
+                                    <p className="hbutton__text">
+                                        <span style={{ '--index': 0 }}>G</span>
+                                        <span style={{ '--index': 1 }}>E</span>
+                                        <span style={{ '--index': 2 }}>T</span>
+                                        <span style={{ '--index': 3 }}></span>
+                                        <span style={{ '--index': 4 }}>S</span>
+                                        <span style={{ '--index': 5 }}>T</span>
+                                        <span style={{ '--index': 6 }}>A</span>
+                                        <span style={{ '--index': 7 }}>R</span>
+                                        <span style={{ '--index': 8 }}>T</span>
+                                        <span style={{ '--index': 9 }}>E</span>
+                                        <span style={{ '--index': 10 }}>D</span>
+                                        <span style={{ '--index': 11 }}> </span>
+                                    </p>
+
+                                    <div className="hbutton__circle">
+                                        <svg viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="hbutton__icon" width="14">
+                                            <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" fill="currentColor"></path>
+                                        </svg>
+                                        <svg viewBox="0 0 14 15" fill="none" width="14" xmlns="http://www.w3.org/2000/svg" className="hbutton__icon hbutton__icon--copy">
+                                            <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" fill="currentColor"></path>
+                                        </svg>
+
+                                    </div>
+                                </button>
                             </Link>
                             <h1>Medq: Your Health, Prioritized</h1>
                         </div>
@@ -230,13 +230,7 @@ const HomePage = () => {
                     <div className="flying-img" style={{ top: '20%', left: '20%', filter: 'blur(0px)' }}>
                         <img src={Agni} alt="Flying Image 1" />
                     </div>
-                    <div className="flying-img" style={{ top: '75%', left: '30%' }}>
-                        <img src={Agni} alt="Flying Image 2" />
-                    </div>
-                    <div className="flying-img" style={{ top: '65%', left: '5%', filter: 'blur(0px)' }}>
-                        <img src={Agni} alt="Flying Image 3" />
-                    </div>
-                    <div className="flying-img" style={{ top: '40%', left: '80%' }}>
+                    <div className="flying-img" style={{ top: '60%', left: '20%' }}>
                         <img src={Agni} alt="Flying Image 4" />
                     </div>
                     <div className="flying-img" style={{ top: '10%', left: '50%', filter: 'blur(0px)' }}>
@@ -291,6 +285,7 @@ const HomePage = () => {
                             Based in India
                         </div>
                     </div>
+
                 </footer>
 
             </div>
