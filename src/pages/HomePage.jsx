74318505c242bef0f87d3093490fbc3bfb282a65
ironkrush <krush2752@gmail.com>
2024-08-29 12:13:55 +0530

    signup page link added to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import HCard2 from '../assets/images/queue_homepage.png'
 import HCard3 from '../assets/images/Bed_homepage.png'
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import '../assets/styles/homepage.css';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     useEffect(() => {
@@ -134,6 +135,7 @@ const HomePage = () => {
                     <div className="medi-img">
                         <div className="tagline">
                             <button className="hbutton">
+                                <Link to='/signup'>
                                 <p className="hbutton__text">
                                     <span style={{ '--index': 0 }}>G</span>
                                     <span style={{ '--index': 1 }}>E</span>
@@ -148,6 +150,7 @@ const HomePage = () => {
                                     <span style={{ '--index': 10 }}>D</span>
                                     <span style={{ '--index': 11 }}> </span>
                                 </p>
+                                </Link>
                                 <div className="hbutton__circle">
                                     <svg viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="hbutton__icon" width="14">
                                         <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" fill="currentColor"></path>
